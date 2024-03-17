c1591da19ac91d391b1895e8f86f932defeae81c
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-03-17 15:08:24 -0400

    response attempt for echo

--- app/server.c
+++ app/server.c
@@ -19,13 +19,16 @@ enum HTTP_METHOD{
     PUT
 };
 
-char *response_good = "HTTP/1.1 200 OK\r\n\r\n";
-char *response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+const char *startline_200 = "HTTP/1.1 200 OK";
+const char *startline_404 = "HTTP/1.1 404 Not Found";
+const char *response_good = "HTTP/1.1 200 OK\r\n\r\n";
+const char *response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
 
 int server_init();
 int server_listen_socket(int *server_fd);
 void server_run(const int server_fd);
-void respond(const int client_fd, char *resposne_body, int response_len);
+void respond(const int client_fd, char *startline, char *headers[], int header_count, char *body);
+void send_response(const int client_fd, char *response_body, int response_len);
 
 void HTTP_GET(int client_fd, char message[], int max_message_length);
 void HTTP_process(int client_fd, char message[], int max_message_length);
@@ -115,7 +118,76 @@ void server_run(const int server_fd){
 //    }
 }
 
-void respond(const int client_fd, char *response_body, int response_len)
+// OLDD
+//void respond(const int client_fd, char *startline, char *headers[], int header_count, char *body){
+//    int bytes_to_allocate = strlen(startline) + 2; // 2 for each <cr><lf> or CRLF or \r\n
+//    for(int i = 0; i < header_count; i++)
+//        bytes_to_allocate += strlen(headers[i]) + 2;
+//    bytes_to_allocate += 2 + strlen(body) + 1; // 1 for null terminator at end
+//
+//    char *message = malloc(bytes_to_allocate);
+//
+//    int i = 0;
+//    while(i < strlen(startline)) {
+//        message[i] = startline[i];
+//        i++;
+//    }
+//    message[i] = '\r';
+//    message[++i] = '\n';
+//
+//    printf("\n%d\n", i);
+//
+//    for(int header = 0; header < header_count; header++)
+//    {
+//        printf("\n%d, %d\n", i, strlen(headers[header]));
+//        int start = i;
+//        while(i < start + strlen(headers[header])) {
+//            message[i] = headers[header][i - start];
+//            i++;
+//        }
+//        message[i] = '\r';
+//        message[++i] = '\n';
+//    }
+//
+//    printf(message);
+//
+//    // format message; use strlen to cull out \0's
+////    snprintf(message, strlen(startline), "%.*s\r\n", strlen(startline), startline);
+////    for(int i = 0; i < header_count; i++)
+////        snprintf(message, bytes_to_allocate,"%.*s\r\n", strlen(headers[i]),  headers[i]);
+////
+////    snprintf(message, bytes_to_allocate, "\r\n%.*s", strlen(body), body);
+//
+//    printf("response message:\r\n%.*s", bytes_to_allocate - 1, message);
+//    send_response(client_fd, message, bytes_to_allocate);
+//    free(message);
+//}
+
+void respond(const int client_fd, char *startline, char *headers[], int header_count, char *body){
+    int bytes_to_allocate = strlen(startline) + 2; // 2 for each <cr><lf> or CRLF or \r\n
+    for(int i = 0; i < header_count; i++)
+        bytes_to_allocate += strlen(headers[i]) + 2;
+    bytes_to_allocate += 2 + strlen(body) + 1; // 1 for null terminator at end
+
+    char *message = malloc(bytes_to_allocate);
+
+//    for(int i = 0; i < strlen(startline); i++)
+//        message[i] = startline[i];
+//
+
+    // format message; use strlen to cull out \0's
+    snprintf(message, strlen(startline), "%.*s\r\n", strlen(startline), startline);
+    for(int i = 0; i < header_count; i++)
+        snprintf(message, bytes_to_allocate,"%.*s\r\n", strlen(headers[i]),  headers[i]);
+
+    snprintf(message, bytes_to_allocate, "\r\n%.*s", strlen(body), body);
+
+    printf("response message:\r\n%.*s", bytes_to_allocate - 1, message);
+    send_response(client_fd, message, bytes_to_allocate);
+    free(message);
+}
+
+void send_response(const int client_fd, char *response_body, int response_len)
 {
     if (send(client_fd, response_body, response_len, 0) <0) {
         printf("Error responding to client: %s\n", strerror(errno));
@@ -146,9 +218,39 @@ void HTTP_GET(int client_fd, char *message, int max_message_length) {
 
 
     if(strncmp(path, "/", path_len - 1) == 0)
-        respond(client_fd, response_good, strlen(response_good));
+        send_response(client_fd, response_good, strlen(response_good));
     else
-        respond(client_fd, response_404, strlen(response_404));
+    {
+        path++; // to cull the first '/'
+        int operation_terminator = 0;
+        while(path[operation_terminator] != '/') operation_terminator++;
+
+        if(strncmp(path, "echo", operation_terminator) == 0)
+        {
+            char *headers[2] = {
+                    "Content-Type: text/plain",
+                    "Content-Length: 3"
+            };
+
+            path += operation_terminator + 1; // cull out / again; rest of path is actual message
+
+            // trim end-of-string other things
+            int body_size = 0;
+            while(path[body_size] != ' ') body_size++;
+            char *body = malloc(body_size);
+            sprintf_s(body, body_size, "%.*s", body_size, path);
+            printf("%d, %.*s\n", body_size,body_size, path);
+            printf("%d, %s\n", body_size, body);
+
+            printf("responding 200 with echo %.*s\n", body_size, body);
+            respond(client_fd, startline_200, headers, 2, body);
+            free(body);
+        }
+
+
+
+        send_response(client_fd, response_404, strlen(response_404));
+    }
 }
 
 // returns -1 on fail; consumes first word if consume param is nonzero
